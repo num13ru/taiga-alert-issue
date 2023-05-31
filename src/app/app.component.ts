cb942d2ba3214bf1c12781eb600145cdf86f1a6e
Eduard Gorte <num13ru@gmail.com>
2023-05-31 13:19:55 +0600

    Add @taiga-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { TuiAlertService } from '@taiga-ui/core';
+import { TuiPushService } from '@taiga-ui/kit';
+import { switchMap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'taiga-alert-issue';
+  constructor(
+    @Inject(TuiPushService) protected readonly push: TuiPushService,
+    @Inject(TuiAlertService) protected readonly alert: TuiAlertService,
+  ) { }
+
+  onClickPush(): void {
+    this.push
+      .open('This is <strong>heavy</strong>!', {
+        heading: 'Great Scott!',
+        type: 'Quote',
+        icon: 'tuiIconVideoLarge',
+        buttons: ['Roads?', '1.21 Gigawatts!?!'],
+      })
+      .pipe(
+        take(1),
+        switchMap(button => this.alert.open(button)),
+      )
+      .subscribe();
+  }
+
+  onClickAlert(): void {
+    this.alert.open('alert').subscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { TuiRootModule, TuiDialogModule, TuiAlertModule } from "@taiga-ui/core";
+import { TuiButtonModule } from '@taiga-ui/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TuiPushModule } from "@taiga-ui/kit";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiPushModule,
+    TuiButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
